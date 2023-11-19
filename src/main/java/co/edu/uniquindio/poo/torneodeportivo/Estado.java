e94136fff422e54dc236dad2aabc2c959b87f8a8
juanpe23 <142370092+JuanLBEnjoyer@users.noreply.github.com>
2023-11-19 15:10:45 -0500

    Delete src directory

--- src/main/java/co/edu/uniquindio/poo/torneodeportivo/Estado.java
+++
@@ -1,6 +0,0 @@
-package co.edu.uniquindio.poo.torneodeportivo;
-
-public enum Estado{
-    PENDIENTE,ENJUEGO,FINALIZADO,APLAZADO
-    
-}
--- src/main/java/co/edu/uniquindio/poo/torneodeportivo/Juez.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.poo.torneodeportivo;
-
-public record Juez(String licencia, String nombre, String apellido, String email, String numero){
-    
-}
